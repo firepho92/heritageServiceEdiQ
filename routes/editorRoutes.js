7162dda38cb6cf64d72b9fa52d10d5f088900377
Alex Aguilar <auza920205@gmail.com>
2020-09-10 20:01:59 -0500

    router putProduct added

--- routes/editorRoutes.js
+++ routes/editorRoutes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
-const { postProduct } = require('../controllers/productsController')
+const { postProduct, putProduct } = require('../controllers/productsController')
 
 const router = Router()
 
 router.post('/products', postProduct)
+routes.put('/products', putProduct)
 
 module.exports = router
